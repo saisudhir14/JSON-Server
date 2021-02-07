9103f14da1a6019bafd698f86173e58773d0170d
sudhirdontha72 <sudhirdontha@outlook.com>
2021-02-08 03:11:50 +0530

    Update details.js

--- js/details.js
+++ js/details.js
@@ -1,1 +1,13 @@
 //JavaScript for details.html
+
+//create a new URL Search params object
+
+const id = new URLSearchParams(window.location.search).get('id');
+
+const renderDetails = async() =>{
+    //to get single endpoint/result 
+    const res = await fetch('http://localhost:3000/posts' + id)
+
+    }
+
+window.addEventListener('DOMContentLoaded',() => renderDetails());
