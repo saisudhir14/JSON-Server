27a402368ce0c7a3571d78e9d5cc7b6d88c25da1
sudhirdontha72 <sudhirdontha@outlook.com>
2021-02-08 02:41:14 +0530

    Created a async function
    used async , await

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 //JavaScript for index.html
 
+const renderPosts = async() =>{
+    //create a URI that makes a request to an endpoint
+    let uri = 'http://localhost:3000/posts';
+    const res = await fetch(uri);
+    const posts = await res.json();
+   // console.log(posts);
+}
+
 window.addEventListener('DOMContentLoaded',() => renderPosts());
