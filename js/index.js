eacb4185f468a81e0a5963f6d35b701b4b849d5b
sudhirdontha72 <sudhirdontha@outlook.com>
2021-02-08 02:46:50 +0530

    Added string template
    Added string template,

--- js/index.js
+++ js/index.js
@@ -6,6 +6,18 @@ const renderPosts = async() =>{
     const res = await fetch(uri);
     const posts = await res.json();
    // console.log(posts);
+
+   //output in DOM now!
+   let template = '';
+   //append the template += and use templateString to output the dynamic String using the template string `` ${}
+    template += `
+     <div class = "post">
+        <h2>${post.title}</h2>
+        <p><small>${post.likes} likes</small></p>
+        <p>${post.body.slice(0,200)}</p>
+        <a href="/details.html"> read more...</a>
+     </div>
+    `
 }
 
 window.addEventListener('DOMContentLoaded',() => renderPosts());
